1b1a75ed0dc7abb9b67c8e350501709b4aa02f77
Atgaras <50113478+Atgaras@users.noreply.github.com>
2019-05-23 01:29:24 +0300

    Add files via upload

---
+++ Struct Islaidos/Struct Islaidos/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_Islaidos
+{
+    class Program
+    {
+        public struct Islaidos
+        {
+            double Maistas;
+            double Buitis;
+            double Mokesciai;
+            double Laisvalaikis;
+            double Saviugda;
+            double Kita;
+
+            public Islaidos(double maistas, double buitis, double mokesciai, double laisvalaikis, double saviugda, double kita)
+            {
+                Maistas = maistas;
+                Buitis = buitis;
+                Mokesciai = mokesciai;
+                Laisvalaikis = laisvalaikis;
+                Saviugda = saviugda;
+                Kita = kita;
+
+            }
+            public int Suma()
+            {
+                return Maistas + Buitis + Mokesciai + Laisvalaikis + Saviugda + Kita;
+            }
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Struct Saugykla Zmogus/Struct Saugykla Zmogus/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_Saugykla_Zmogus
+{
+    class Program
+    {
+        public struct Zmogus
+        {
+            public string Vardas;
+            public string Pavarde;
+            public DateTime GimimoData;
+
+            public Zmogus(string vardas, string pavarde, DateTime gimimoData)
+            {
+                Vardas = vardas;
+                Pavarde = pavarde;
+                GimimoData = gimimoData;
+            }
+        }
+        static void Main(string[] args)
+        {
+            List < Zmogus > listas = new List<Zmogus>();
+            Console.WriteLine("Įveskite savo vardą:");
+            Zmogus zm1 = new Zmogus();
+            zm1.Vardas = Console.ReadLine();
+            Console.WriteLine("Įveskite savo pavardę:");
+            Zmogus zm01 = new Zmogus();
+            zm01.Pavarde = Console.ReadLine();
+            Console.WriteLine("Įveskite savo amžių:");
+            Zmogus zm001 = new Zmogus();
+            zm001.GimimoData = DateTime(Console.ReadLine());
+            
+            listas.Add(zm1);
+            Console.ReadLine();
+        }
+    }
+}
