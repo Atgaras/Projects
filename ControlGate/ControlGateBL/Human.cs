43ec0f95d361b5065b92cfb9e718bcca4a96727f
Atgaras <goferis.e@gmail.com>
2019-06-17 22:08:04 +0300

    Lesson 14

---
+++ ControlGate/ControlGateBL/Human.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ControlGateBL
+{
+    public class Human
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public List<int> GateIdRights { get; set; }
+    }
+}
---
+++ ControlGate/ControlGateBL/HumanRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ControlGateBL
+{
+    public class HumanRepository
+    {
+        private List<Human> humans = new List<Human>();
+        public HumanRepository()
+        {
+            humans.Add(new Human());
+        }
+    }
+}
